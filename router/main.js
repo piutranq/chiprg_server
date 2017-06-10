3b068dd72fe8f3d823bca00df71ea583a01f9556
Piu Tranquillo <piutranq@hotmail.com>
2017-06-10 18:28:52 +0900

    get요청을 받으면 db에서 데이터를 꺼내주는 기능을 간단히 구현.

--- router/main.js
+++ router/main.js
@@ -1,9 +1,29 @@
 module.exports = function(app)
 {
-     app.get('/',function(req,res){
-        res.send('Hello');
-     });
-     app.get('/user',function(req,res){
-        res.send('User');
+  app.get('/',function(req,res){
+    res.send('Hello');
+  });
+  app.get('/user',function(req,res){
+    var ret;
+    getUser().then(function(resolve){
+      ret = resolve;
+      res.send(ret);
     });
+  });
 }
+
+var getUser = function(param){
+  return new Promise(function(resolve, reject){
+    var MongoClient = require('mongodb').MongoClient;
+    var dbPath = "mongodb://localhost:27017/chiprg";
+    MongoClient.connect(dbPath, function(err, db){
+      if(err) throw err;
+      console.log("Database Connected");
+      db.collection("userData").find({}).toArray(function(err, res){
+        if(err) throw err;
+        resolve(res);
+      });
+      db.close();
+    });
+  });
+};
--- server.js
+++ server.js
@@ -3,5 +3,20 @@ var app = express();
 var router = require('./router/main')(app);
 
 var server = app.listen(21003, function(){
-    console.log("Express server has started on port 21003")
+  console.log("Express server has started on port 21003")
 });
+
+/*
+var _promise = function(param){
+	return new Promise(function(resolve, reject){
+    console.log('_promise Start');
+		setTimeout(function(){
+			resolve('OK');
+    }, 3000);
+  });
+};
+
+_promise(true).then(function(resolve){
+  console.log('YEAH' + resolve)
+});
+*/
