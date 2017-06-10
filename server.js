0ee7863b1383f28c631c652c14f69b601d271584
Piu Tranquillo <piutranq@hotmail.com>
2017-06-10 15:27:45 +0900

    간단한 router 구현

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var router = require('./router/main')(app);
+
 var server = app.listen(21003, function(){
     console.log("Express server has started on port 21003")
 });
