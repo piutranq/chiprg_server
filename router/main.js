35e10bbfa85580a4de39e0f0744a20cb062d5adf
Piu Tranquillo <piutranq@hotmail.com>
2017-06-14 13:50:26 +0900

    enable role-based authentication on database

--- router/main.js
+++ router/main.js
@@ -1,28 +1,49 @@
+
+// require
+var MongoClient = require('mongodb').MongoClient;
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+
+// const
+var DBPATH = "mongodb://studio2aoe:t1emppa47ssw883ord@localhost:27017/chiprg";
+
 module.exports = function(app)
 {
+  
   app.get('/',function(req,res){
-    res.send('Hello');
+    res.send('GAME');
   });
+
   app.get('/user',function(req,res){
-    var ret;
-    getUser().then(function(resolve){
-      ret = resolve;
-      res.send(ret);
+    promiseDBConnect(DBPATH).
+      then(function(resolve) { return promiseGetUser(resolve); }).
+      then(function(resolve) { res.send(resolve);});
+  });
+
+  app.get('/freeranking', function(req, res){
+    res.send('GET /freeranking');
+  });
+
+  app.post('/freeranking', function(req, res){
+    console.log(req);
+    res.send('POST /freeranking');
+  });
+};
+
+var promiseDBConnect = function(dbPath){
+  return new Promise(function(resolve, reject){
+    MongoClient.connect(dbPath, function(err, res){
+      if(err) throw err;
+      resolve(res);
     });
   });
-}
+};
 
-var getUser = function(param){
+var promiseGetUser = function(db){
   return new Promise(function(resolve, reject){
-    var MongoClient = require('mongodb').MongoClient;
-    var dbPath = "mongodb://localhost:27017/chiprg";
-    MongoClient.connect(dbPath, function(err, db){
+    db.collection("userData").find({}).toArray(function(err, res){
       if(err) throw err;
-      console.log("Database Connected");
-      db.collection("userData").find({}).toArray(function(err, res){
-        if(err) throw err;
-        resolve(res);
-      });
+      resolve(res);
       db.close();
     });
   });
